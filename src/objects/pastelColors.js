f229266c7d36b7a7d7788ecd90d64dc28f358015
Pukkok <krystalgggg@gmail.com>
2025-07-02 17:26:17 +0900

    [Feat] 게임색상 객체 생성

---
+++ src/objects/pastelColors.js
@@ -0,0 +1,30 @@
+export const pastelColors = {
+  softRed: 0xf28b82,      // 연한 빨강
+  softOrange: 0xfbbc04,   // 연한 주황
+  softYellow: 0xfff475,   // 연한 노랑
+  softGreen: 0xccff90,    // 연한 초록
+  softTeal: 0xa7ffeb,     // 연한 청록
+  softBlue: 0xcbf0f8,     // 연한 파랑
+  softPurple: 0xd7aefb,   // 연한 보라
+  softPink: 0xfdcfe8,     // 연한 분홍
+  softBrown: 0xe6c9a8,    // 연한 갈색
+  softGray: 0xe8eaed,     // 연한 회색
+  softMint: 0xb5ead7,     // 연한 민트
+  softLavender: 0xd0bfff  // 연한 라벤더
+}
+
+export const darkPastelColors = {
+  softRed: 0xc75a5a,       // 좀 더 붉고 진하게
+  softOrange: 0xd97c4c,    // 좀 더 주황 느낌 강조
+  softYellow: 0xe3ca4b,    // 머스타드에서 살짝 밝게
+  softGreen: 0x6b8f5b,     // 더 진한 다크 그린
+  softTeal: 0x4b8a8d,      // 청록 느낌 살리고 진하게
+  softBlue: 0x4a6a8a,      // 그레이시 블루에서 더 파랑쪽으로
+  softPurple: 0x7d5590,    // 바이올렛 진하게
+  softPink: 0xba5e7a,      // 핑크톤 더 선명하게
+  softBrown: 0x8b6f57,     // 브라운 톤 좀 더 깊게
+  softGray: 0x7a7a7a,      // 회색 좀 더 짙게
+  softMint: 0x56877a,      // 민트 진한 톤으로
+  softLavender: 0x67537e   // 라벤더 조금 더 푸른 느낌
+}
+
