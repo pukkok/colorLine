fa3cc4211e2b57cda8e087e8132b5ae37e98c2b3
Pukkok <krystalgggg@gmail.com>
2025-07-02 18:26:20 +0900

    [Feat] 게임 작동 확인

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import './style.css'
 import Phaser from 'phaser'
+import GameScene from './scenes/GameScene'
+import MenuScene from './scenes/MenuScene'
 
 const config = {
   type: Phaser.AUTO,
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: '#1d1d1d',
-  scene: {
-    create() {
-      this.add.text(200, 300, 'Hello Phaser!', {
-        fontSize: '32px',
-        color: '#ffffff'
-      }).setOrigin(0.5)
-    }
-  }
+  scene: [MenuScene, GameScene]
 }
 
 new Phaser.Game(config)
