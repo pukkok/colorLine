49459bfb03bc59b236f821599154c24837e4764e
Pukkok <krystalgggg@gmail.com>
2025-07-02 17:26:01 +0900

    [Feat] 게임세팅 객체 생성

---
+++ src/objects/gameSetting.js
@@ -0,0 +1,20 @@
+export const difficultySettings = {
+  easy: {
+    tubeCount: 6,
+    tubeCapacity: 6,
+    colorCount: 6,
+    emptyTubeCount: 2,
+  },
+  normal: {
+    tubeCount: 8,
+    tubeCapacity: 8,
+    colorCount: 8,
+    emptyTubeCount: 2,
+  },
+  hard: {
+    tubeCount: 10,
+    tubeCapacity: 10,
+    colorCount: 10,
+    emptyTubeCount: 3,
+  }
+}
